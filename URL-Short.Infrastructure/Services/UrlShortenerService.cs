f586984caaa40b02abd05c7db4cfcfac69e9b7f8
RayanAlsh <Ralshanaiber@sure.com.sa>
2024-05-21 11:29:17 +0300

    generatedurl from L = 8 to L = 6

--- URL-Short.Infrastructure/Services/UrlShortenerService.cs
+++ URL-Short.Infrastructure/Services/UrlShortenerService.cs
@@ -5,7 +5,7 @@ public class UrlShortenerService
     private static readonly Random Random = new Random();
     private const string Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    public string GenerateShortUrl(int length = 8)
+    public string GenerateShortUrl(int length = 6)
     {
         return new string(Enumerable.Repeat(Chars, length)
             .Select(s => s[Random.Next(s.Length)]).ToArray());
